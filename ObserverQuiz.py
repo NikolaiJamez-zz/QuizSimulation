37771cf274cd77be04cb4405ed8c075c6174cafb
Nicholas James <nikolaijamez@gmail.com>
2019-03-20 19:34:05 +1300

    Add commits for clarity

--- ObserverQuiz.py
+++ ObserverQuiz.py
@@ -9,11 +9,12 @@ QUESTION_INDEX = 0
 ANSWER_INDEX = 1
 TIME_LIMIT = 20
 URL = "https://opentdb.com/api.php?amount={}&type=multiple".format(QUESTION_NUM)
-NAME_LOCATION = "../Resources/RandomNames.txt"
+NAME_LOCATION = "../Resources/RandomNames.txt"  # Gets random contestant names from file
 PLAYERS = 5
 SEPARATOR = "=========="
 
-
+# Creates the single quiz-master, chosen number of players with random names,
+# questions to be read and plays as many rounds as there are questions
 class Simulation:
     def __init__(self):
         self.q = Question()
@@ -27,6 +28,7 @@ class Simulation:
             self.play_round()
         self.display_winner()
 
+    # Gets names from file and shuffles
     @staticmethod
     def get_names():
         file = open(NAME_LOCATION, "r")
@@ -37,6 +39,10 @@ class Simulation:
         file.close()
         return names
 
+    # Controls everything needed to play a round including:
+    # Forming a question and displaying it to the contestants
+    # Create a thread for each contestant so they can "think" at different times
+    # Display the contestants that got the answer correct 
     def play_round(self):
         self.qm.form_question()
         self.qm.display_question(self.qm.question, self.qm.all_answers)
@@ -46,6 +52,7 @@ class Simulation:
         print("{} All players have answered {}".format(SEPARATOR, SEPARATOR))
         self.qm.display_winners()
 
+    # Sorts tghrough all the winners and displays them in order of first to last
     def display_winner(self):
         winners = [self.p[0]]
         first = True
@@ -63,7 +70,7 @@ class Simulation:
             print(player.name)
         print(SEPARATOR * 10)
 
-
+# thread used to allow contestants to "think" at different speeds
 class DecideThread(threading.Thread):
     def __init__(self, p, a):
         threading.Thread.__init__(self)
@@ -76,11 +83,12 @@ class DecideThread(threading.Thread):
         self.player.choose_answer(self.a)
 
 
+# Handles getting and making the question readbale
 class Question:
     def __init__(self):
         self.questions = []
         self.make_questions()
-
+    # Gets questions from Open Trivia DB and sorts them into an array
     def make_questions(self):
         self.questions.clear()
         r = urllib.request.urlopen(URL)
@@ -101,6 +109,7 @@ class Question:
         return len(self.questions)
 
 
+# Handles everything a contestant needs to play
 class Player:
     def __init__(self, quiz_master, name):
         self.quiz_master = quiz_master
@@ -109,10 +118,12 @@ class Player:
         self.answer = ""
         self.wins = 0
 
+    # Send answer to quiz master
     def choose_answer(self, a):
         self.quiz_master.receive_answer(a[random.randrange(len(a))], self)
 
 
+# Handles everything the quiz master needs to run the game
 class QuizMaster:
     def __init__(self, q):
         self.players = []
@@ -123,9 +134,11 @@ class QuizMaster:
         self.all_answers = []
         self.threads = []
 
+    # Registers a player
     def register_observer(self, o):
         self.players.append(o)
 
+    # Forms the question and answers
     def form_question(self):
         self.answers.clear()
         data = self.q.get_question()
@@ -133,18 +146,22 @@ class QuizMaster:
         self.all_answers = data[ANSWER_INDEX]
         self.answer = data[1][0]
 
+    # Displays question to contestants
     def display_question(self, q, a):
         print("{}\nQuestion {}\n{}\n{}\n[1] {}    [2] {}    [3] {}    [4] {}\n{}".format(SEPARATOR * 10, QUESTION_NUM - self.q.get_questions_left(), SEPARATOR, q, a[0], a[1], a[2], a[3], SEPARATOR))
 
+    # Gives each player a thread and provides them with the question/answers
     def give_question(self, a):
         self.threads.clear()
         for player in range(PLAYERS):
             self.threads.append(DecideThread(self.players[player], a))
             self.threads[player].start()
 
+    # Recieves answer from contestant
     def receive_answer(self, a, player):
         self.answers.append([a, player])
 
+    # Displays who was correct and the answer
     def display_winners(self):
         for answer in self.answers:
             if answer[0] == self.answer:
@@ -156,5 +173,5 @@ class QuizMaster:
     def remove_observers(self):
         self.observers.clear()
 
-
+# Starts simulation
 s = Simulation()
